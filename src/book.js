963b8c88aa913d1ebce0212beb6aa5b207179080
Hanna <hannaeberhoefer@yahoo.com>
2020-08-06 15:45:22 +0100

    Display books

--- src/book.js
+++ src/book.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const libraryBooks = document.querySelector('#library-books');
+
 let myLibrary = []
 
 function Book(title, author, pages, isRead = false) {
@@ -20,5 +22,18 @@ function addBookToLibrary() {
   let book = new Book(title, author, pages, isRead)
   return myLibrary.push(book);
 };
+
+function render(myLibrary) {
+  libraryBooks.innerHTML = '';
+  for (let i = 0; i < myLibrary.length; i++) {
+    libraryBooks.innerHTML +=
+      `<tr index="${i}">
+        <td>${myLibrary[i].title}</td>
+        <td>${myLibrary[i].author}</td>
+        <td>${myLibrary[i].pages}</td>
+        <td>${myLibrary[i].isRead}</td>
+      </tr>`;
+  }
+}
 
 // var book = new Book('Game of Thrones', 'George R. R. Martin', 651, true);
